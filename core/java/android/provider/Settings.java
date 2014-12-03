7772e6043e5b4914d755852a9393513b0a27efd1
Lars Greiss <kufikugel@googlemail.com>
2014-12-03 19:56:41 +0100

    Return: Smart Pulldown [1/2][Squashed]
    Rewritten by @Beanstown106 for 6.0/7.0
    
    previously the detection for ongoing/persistent notifications were not working
    im not sure how long this has actually been broken but i cleaned things up a little bit
    also put to use the isOngoing boolean and properly put to use the hasActiveVisibleNotifications boolean
    
    current options now are off, no dismissable, no ongoing, and no notifications
    
    Credits: kufikugel, HardCorePawn, acardinal
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3717,6 +3717,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Quick Settings Smart Pulldown
+         * @hide
+         */
+        public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
