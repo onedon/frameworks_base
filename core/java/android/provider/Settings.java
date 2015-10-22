e5ba0f746916b1cd7e88b54a93dc73692f15163b
Danesh M <daneshm90@gmail.com>
2015-10-22 10:51:02 -0700

    SystemUI : Clock position support + am/pm styles (1/2)
    Based on Dave Kessler's work from cm-12.0
    
    Change-Id: I88e630fcfcffd8a795f1abbeecd71a258d0e8e81
    
    SystemUI: recalculate clock position on removing empty shade views
    Change-Id: I70cc2dd3e1da99d4467cd43fc81018ccf135cd77
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: fix clock jumping when expanding QS from lockscreen
    commit 4f965c5 fixed recalculating clock position from shade_locked but
    it didn't account for other scenarios such as when the QS is expanding,
    causing the clock to jump when expanding QS from lockscreen.
    
    Change-Id: I0e4f473fbc500f7dcdfa015cbedda07761c3cdb2
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI : Fix left/center clock visibility
    Changes for Nougat remove clock tuner changes by @camcory
    Change-Id: I5bee0927ceaf93ea4cc0751bcb866aaaed64356b
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3824,6 +3824,28 @@ public final class Settings {
         public static final String QS_COLUMNS_LANDSCAPE = "qs_columns_landscape";
 
         /**
+         * Whether to hide the clock, show it in the right or left
+         * position or show it in the center
+         * 0: don't show the clock
+         * 1: show the clock in the right position (LTR)
+         * 2: show the clock in the center
+         * 3: show the clock in the left position (LTR)
+         * default: 1
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK = "status_bar_clock";
+
+        /**
+         * Display style of AM/PM next to clock in status bar
+         * 0: Normal display (Eclair stock)
+         * 1: Small display (Froyo stock)
+         * 2: No display (Gingerbread/ICS stock)
+         * default: 2
+         * @hide
+         */
+        public static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -3879,7 +3901,9 @@ public final class Settings {
             NOTIFICATION_SOUND,
             ACCELEROMETER_ROTATION,
             NOTIFICATION_LIGHT_SCREEN_ON,
-            ALLOW_LIGHTS
+            ALLOW_LIGHTS,
+            STATUS_BAR_CLOCK,
+            STATUS_BAR_AM_PM
         };
 
         /**
